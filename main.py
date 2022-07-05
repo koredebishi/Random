1cb0526c99271777ffe5271f00ceb27f2c34ccdd
MrKaydee <44779363+koredebishi@users.noreply.github.com>
2022-07-05 12:27:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import my_module
+a =200
+b = 400
+print (my_module.pi)
+int_random = random.randint(a,b)
+print (int_random)
+float_random = random.random() * 5
+print (float_random)
+#int_random = random.randint(1,20)
+#print ("Integer is", int_random)
